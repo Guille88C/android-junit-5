c8adc0f45057e90d643e7c8d7038588112a5547a
Guillermo Cuesta <guillermo.cuesta@feverup.com>
2023-03-27 19:50:05 +0200

    Create more parametrized tests

--- app/src/test/java/com/guille88c/junit5/ExampleUnitTest.kt
+++ app/src/test/java/com/guille88c/junit5/ExampleUnitTest.kt
@@ -1,5 +1,7 @@
 package com.guille88c.junit5
 
+import java.util.Arrays
+import java.util.stream.Stream
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
@@ -8,6 +10,9 @@ import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.MethodSource
 import org.junit.jupiter.params.provider.ValueSource
 
 /**
@@ -64,6 +69,33 @@ internal class ExampleUnitTest {
     }
 
     @Tag("enable_test")
+    @CsvSource(
+        "2, 1, 2",
+        "2, 2, 1",
+        "4, 1, 4",
+        "4, 2, 2",
+        "4, 4, 1"
+    )
+    @ParameterizedTest
+    fun testDiv(num: Int, den: Int, res: Int) {
+        assertEquals(res, num.div(den))
+    }
+
+    @Tag("enable_test")
+    @MethodSource("arguments")
+    @ParameterizedTest
+    fun testTimesWithArguments(first: Int, second: Int, res: Int) {
+        assertEquals(res, first.times(second))
+    }
+
+    @Tag("enable_test")
+    @MethodSource("myArguments")
+    @ParameterizedTest
+    fun testTimesWithMyArguments(myArguments: MyArguments) {
+        assertEquals(myArguments.res, myArguments.first.times(myArguments.second))
+    }
+
+    @Tag("enable_test")
     @DisplayName("Given zero number")
     @Nested
     inner class GivenZeroNumber {
@@ -85,4 +117,31 @@ internal class ExampleUnitTest {
             assertEquals(0, 2.times(number))
         }
     }
+
+    companion object {
+
+        @JvmStatic
+        fun arguments(): Stream<Arguments> = Stream.of(
+            Arguments.of(1, 1, 1),
+            Arguments.of(1, 2, 2),
+            Arguments.of(2, 2, 4),
+            Arguments.of(2, 3, 6)
+        )
+
+        @JvmStatic
+        fun myArguments(): Stream<MyArguments> = Arrays.stream(
+            arrayOf(
+                MyArguments(1, 1, 1),
+                MyArguments(1, 2, 2),
+                MyArguments(2, 2, 4),
+                MyArguments(2, 3, 6)
+            )
+        )
+    }
+
+    data class MyArguments(
+        val first: Int,
+        val second: Int,
+        val res: Int
+    )
 }
